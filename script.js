e8b6c42653d318ad76685d1f61af9f3d484da36c
Misael-GC <misael.gomez1623@gmail.com>
2022-08-08 22:33:43 -0500

    form

--- script.js
+++ script.js
@@ -4,9 +4,28 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pRseult = document.querySelector('#result');
 
+// btn.addEventListener('submit', btnOnClick);
+
+// function btnOnClick(event){
+//     console.log({event});
+//     event.preventDefault();
+//     const sumaInputs = Number(input1.value) + Number(input2.value);
+//     pRseult.innerText = `Resultado: ${sumaInputs}`;
+// }
+
 btn.addEventListener('click', btnOnClick);
 
-function btnOnClick(){
+function btnOnClick(event){
+    // console.log({event});
+    // event.preventDefault();
     const sumaInputs = Number(input1.value) + Number(input2.value);
     pRseult.innerText = `Resultado: ${sumaInputs}`;
 }
+
+// const textToCopy = document.querySelector('#textToCopy');
+
+// textToCopy.addEventListener('copy', showMessageWhenCopy);
+
+// function showMessageWhenCopy(){
+//     alert('copy the text cap!!!')
+// }
